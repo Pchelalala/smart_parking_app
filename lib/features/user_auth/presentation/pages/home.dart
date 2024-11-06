288969527ee38b100376eac2e4c50e70d0b380b3
Pchela <lyudmilalittlepchela@gmail.com>
2024-11-06 20:29:18 +0200

    add user profile screen with static data

--- lib/features/user_auth/presentation/pages/home.dart
+++ lib/features/user_auth/presentation/pages/home.dart
@@ -1,9 +1,5 @@
-import 'dart:ffi';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 import '../../../../global/common/toast.dart';
 
@@ -15,179 +11,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Static user data for now
+  final String profileImageUrl = 'https://via.placeholder.com/150';
+  final String firstName = 'John';
+  final String lastName = 'Doe';
+  final String carPlates = 'ABC 123';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: const Text("HomePage"),
+          title: const Text('Profile'),
+          backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  _createData(UserModel(
-                    username: "Henry",
-                    age: 21,
-                    adress: "London",
-                  ));
-                },
-                child: Container(
-                  height: 45,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: const Center(
-                    child: Text(
-                      "Create Data",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
+        body: SingleChildScrollView(
+            child: Center(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 40.0, horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(profileImageUrl),
+                  backgroundColor: Colors.blue.shade100,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  '$firstName $lastName',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              StreamBuilder<List<UserModel>>(
-                  stream: _readData(),
-                  builder: (context, snapshot) {
-                    if(snapshot.connectionState == ConnectionState.waiting){
-                      return const Center(child: CircularProgressIndicator(),);
-                    } if(snapshot.data!.isEmpty){
-                      return const Center(child:Text("No Data Yet"));
-                    }
-                    final users = snapshot.data;
-                    return Padding(padding: const EdgeInsets.all(8),
-                      child: Column(
-                          children: users!.map((user) {
-                            return ListTile(
-                              leading: GestureDetector(
-                                onTap: (){
-                                  _deleteData(user.id!);
-                                },
-                                child: const Icon(Icons.delete),
-                              ),
-                              trailing: GestureDetector(
-                                onTap: (){
-                                  _updateData(
-                                      UserModel(
-                                        id: user.id,
-                                        username: "John Wick",
-                                        adress: "Pakistan",)
-                                  );
-                                },
-                                child: Icon(Icons.update),
-                              ),
-                              title: Text(user.username!),
-                              subtitle: Text(user.adress!),
-                            );
-                          }).toList()
-                      ),);
-                  }
-              ),
-
-              GestureDetector(
-                onTap: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pushNamed(context, "/login");
-                  showToast(message: "Successfully signed out");
-                },
-                child: Container(
-                  height: 45,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: const Center(
-                    child: Text(
-                      "Sign out",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
+                Text(
+                  'Car Plates: $carPlates',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.blue,
                   ),
                 ),
-              )
-            ],
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    // Your code for edit profile
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'Edit Profile',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    // Your code for viewing parking history
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text(
+                    'View Parking History',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.pushNamed(context, "/login");
+                    showToast(message: "Successfully signed out");
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: const Text(
+                    'Sign Out',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ));
-  }
-
-  Stream<List<UserModel>> _readData(){
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    return userCollection.snapshots().map((qureySnapshot)
-    => qureySnapshot.docs.map((e)
-    => UserModel.fromSnapshot(e),).toList());
-  }
-
-  void _createData(UserModel userModel) {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    String id = userCollection.doc().id;
-
-    final newUser = UserModel(
-      username: userModel.username,
-      age: userModel.age,
-      adress: userModel.adress,
-      id: id,
-    ).toJson();
-
-    userCollection.doc(id).set(newUser);
-  }
-
-  void _updateData(UserModel userModel) {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    final newData = UserModel(
-      username: userModel.username,
-      id: userModel.id,
-      adress: userModel.adress,
-      age: userModel.age,
-    ).toJson();
-
-    userCollection.doc(userModel.id).update(newData);
-
-  }
-
-  void _deleteData(String id) {
-    final userCollection = FirebaseFirestore.instance.collection("users");
-
-    userCollection.doc(id).delete();
-
-  }
-
-}
-
-class UserModel{
-  final String? username;
-  final String? adress;
-  final int? age;
-  final String? id;
-
-  UserModel({this.id,this.username, this.adress, this.age});
-
-
-  static UserModel fromSnapshot(DocumentSnapshot<Map<String, dynamic>> snapshot){
-    return UserModel(
-      username: snapshot['username'],
-      adress: snapshot['adress'],
-      age: snapshot['age'],
-      id: snapshot['id'],
-    );
-  }
-
-  Map<String, dynamic> toJson(){
-    return {
-      "username": username,
-      "age": age,
-      "id": id,
-      "adress": adress,
-    };
+        )));
   }
 }
