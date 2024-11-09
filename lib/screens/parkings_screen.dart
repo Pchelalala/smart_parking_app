eceff0768c0cefcb457f9675031ee10bc36b3977
Pchela <lyudmilalittlepchela@gmail.com>
2024-11-09 23:26:30 +0200

    fetching parking places from firestore

--- lib/screens/parkings_screen.dart
+++ lib/screens/parkings_screen.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_parking_app/screens/parking_spots_screen.dart';
+import '../models/parking_place_model.dart';
 
 class ParkingsScreen extends StatefulWidget {
   const ParkingsScreen({super.key});
@@ -9,29 +11,6 @@ class ParkingsScreen extends StatefulWidget {
 }
 
 class _ParkingsScreenState extends State<ParkingsScreen> {
-  // Sample static data for parking places; replace with dynamic data when available
-  final List<Map<String, String>> parkingPlaces = [
-    {
-      'imageUrl':
-          'https://media.istockphoto.com/id/480652712/photo/dealer-new-cars-stock.jpg?s=612x612&w=0&k=20&c=Mzfb5oEeovQblEo160df-xFxfd6dGoLBkqjjDWQbd5E=',
-      'address': 'Laisvės al. 1, Kaunas',
-    },
-    {
-      'imageUrl':
-          'https://preview.redd.it/bkcvp1okq7081.jpg?width=1080&crop=smart&auto=webp&s=40c0c799d2b82c4266942b04c0df009e0e0b05f7',
-      'address': 'Vilniaus g. 23, Kaunas',
-    },
-    {
-      'imageUrl': 'https://i.imgur.com/BC6PSsS.jpg',
-      'address': 'Karaliaus Mindaugo pr. 50, Kaunas',
-    },
-    {
-      'imageUrl':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4tpDFYZpIeI4AEw8Df0p-JnQQGB5XFOiDCA&s',
-      'address': 'Savanorių pr. 15, Kaunas',
-    },
-  ];
-
   void _navigateToProfile() {
     Navigator.pushNamed(context, '/home');
   }
@@ -61,22 +40,42 @@ class _ParkingsScreenState extends State<ParkingsScreen> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: parkingPlaces.length,
-        itemBuilder: (context, index) {
-          final parking = parkingPlaces[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            child: ListTile(
-              leading: Image.network(
-                parking['imageUrl']!,
-                width: 60,
-                height: 60,
-                fit: BoxFit.cover,
-              ),
-              title: Text(parking['address']!),
-              onTap: () => _navigateToParkingSpotsScreen(parking['address']!),
-            ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream:
+            FirebaseFirestore.instance.collection('parking_places').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No parking places available.'));
+          }
+
+          final parkingPlaces = snapshot.data!.docs.map((doc) {
+            return ParkingPlaceModel.fromSnapshot(
+                doc as DocumentSnapshot<Map<String, dynamic>>);
+          }).toList();
+
+          return ListView.builder(
+            itemCount: parkingPlaces.length,
+            itemBuilder: (context, index) {
+              final parking = parkingPlaces[index];
+              return Card(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: ListTile(
+                  leading: Image.network(
+                    parking.imageUrl ?? '',
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(parking.address ?? 'Unknown Address'),
+                  onTap: () =>
+                      _navigateToParkingSpotsScreen(parking.address ?? ''),
+                ),
+              );
+            },
           );
         },
       ),
