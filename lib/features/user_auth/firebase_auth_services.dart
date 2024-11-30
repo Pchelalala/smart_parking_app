2a8ac818c5e7f295f94e1d0c3813ea1faa172fb0
Pchela <lyudmilalittlepchela@gmail.com>
2024-11-30 23:40:02 +0200

    add password policy

--- lib/features/user_auth/firebase_auth_services.dart
+++ lib/features/user_auth/firebase_auth_services.dart
@@ -4,9 +4,31 @@ import '../../components/toast.dart';
 class FirebaseAuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  bool _validatePassword(String password) {
+    final bool hasUppercase = password.contains(RegExp(r'[A-Z]'));
+    final bool hasLowercase = password.contains(RegExp(r'[a-z]'));
+    final bool hasNumeric = password.contains(RegExp(r'[0-9]'));
+    final bool hasSpecialCharacter =
+        password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+    final bool isLengthValid = password.length >= 8;
+
+    return hasUppercase &&
+        hasLowercase &&
+        hasNumeric &&
+        hasSpecialCharacter &&
+        isLengthValid;
+  }
+
   Future<User?> signUpWithEmailAndPassword(
       String email, String password) async {
     try {
+      if (!_validatePassword(password)) {
+        showToast(
+            message: 'Password must be at least 8 characters long, contain '
+                'uppercase, lowercase, a number, and a special character.');
+        return null;
+      }
+
       UserCredential credential = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       return credential.user;
